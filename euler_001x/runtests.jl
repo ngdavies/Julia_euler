b3ddcee355e7d803f091b845c93772f22b617233
ngdavies@hotmail.com <ngdavies@hotmail.com>
2025-03-14 18:11:20 +0000

    Euler in Julia exercise 0003 passes all tests.

--- euler_001x/runtests.jl
+++ euler_001x/runtests.jl
@@ -3,6 +3,11 @@
 using Test
 
 include("euler_001x.jl")
+@testset "euler_0003_ex" begin
+    @test euler_0003(13195) == 29
+    @test euler_0003(600851475143) == 6857
+end
+
 @testset "euler_0002_ex" begin
     @test euler_0002(100) == 44
     @test euler_0002(4000000) == 4613732

9d7d7461c3a55747cdce789bb995465272214d05
ngdavies@hotmail.com <ngdavies@hotmail.com>
2025-03-14 17:55:01 +0000

    Euler 0002 all tests pass in Julia

--- euler_001x/runtests.jl
+++ euler_001x/runtests.jl
@@ -3,6 +3,10 @@
 using Test
 
 include("euler_001x.jl")
+@testset "euler_0002_ex" begin
+    @test euler_0002(100) == 44
+    @test euler_0002(4000000) == 4613732
+end
 
 @testset "euler_0001_ex" begin
     @test euler_0001(10) == 23 

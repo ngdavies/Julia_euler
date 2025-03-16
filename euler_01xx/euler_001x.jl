f293f877a6d310aa8d8545336a248db90cdd6374
ngdavies@hotmail.com <ngdavies@hotmail.com>
2025-03-16 19:38:10 +0000

    Euler Julia 0018 triangle minumum path sum in progress.

--- euler_01xx/euler_001x.jl
+++ euler_01xx/euler_001x.jl
@@ -1,5 +1,68 @@
 #!/usr/bin/env julia
 
+# Euler_0018 Maximum path sum I
+# By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+# 3
+# 7 4
+# 2 4 6
+# 8 5 9 3
+# That is, 3 + 7 + 4 + 9 = 23.
+# Find the maximum total from top to bottom of the triangle below:
+const triangle_data = """
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+"""
+
+const test_triangle_data = """
+3
+7 4
+2 4 6
+8 5 9 3
+"""
+
+
+function euler_0018(test)
+    if test == true
+        triangle_string = test_triangle_data
+    else
+        triangle_string = triangle_data
+    end
+    triangle = []
+    for line in split(triangle_string, '\n')
+        if length(line) != 0
+            push!(triangle, [parse(Int, x) for x in split(line)])
+        end
+    end
+    nRows = length(triangle)
+    maxCols = length(triangle[nRows])
+    for row in 2:nRows
+        for col in 1:row 
+            if col == 1
+                triangle[row][col] += triangle[row-1][col]
+            elseif col == row
+                triangle[row][col] += triangle[row-1][col-1]
+            else
+                triangle[row][col] += maximum(triangle[row-1][col-1], triangle[row-1][col])
+            end
+        end
+    end
+    return maximum(triangle[nRows])
+end
+
+
 # Euler_0017 Number letter counts
 # If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are
 # 3 + 3 + 5 + 4 + 4 = 19 letters used in total.

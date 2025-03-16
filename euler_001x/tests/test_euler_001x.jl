c8a84e77f736cbc348a36947087a445cb3f9a78a
ngdavies@hotmail.com <ngdavies@hotmail.com>
2025-03-16 17:29:41 +0000

    Julia Euler 0012 passes both test. Copilot with my improvments.

--- euler_001x/tests/test_euler_001x.jl
+++ euler_001x/tests/test_euler_001x.jl
@@ -4,10 +4,14 @@ using Test
 
 include("../euler_001x.jl")
 
-# @testset "Euler_0011" begin
-#     @test euler_0011(4) == 70600674
-#     @test euler_0011(20) == 70600674
-# end
+@testset "Euler_0012" begin
+    @test euler_0012(5) == 28
+    @test euler_0012(500) == 76576500
+end
+
+@testset "Euler_0011" begin
+    @test euler_0011() == 70600674
+end
 
 @testset "Euler_0010" begin
     @test euler_0010(10) == 17

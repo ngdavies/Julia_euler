2cfad1b559c88c4e74f747a79ab7be9752034ae4
ngdavies@hotmail.com <ngdavies@hotmail.com>
2025-03-20 16:45:52 +0000

    Euler, Julia 0021 Amicable numbers, tests pass, optimisation possible.

--- euler_01xx/euler_002x.jl
+++ euler_01xx/euler_002x.jl
@@ -1,5 +1,28 @@
 #!/usr/bin/env julia
 
+# Euler_0021 Amicable numbers
+# Let d(n) be defined as the sum of proper divisors of n 
+# (numbers less than n which divide evenly into n).
+# If d(a) = b and d(b) = a, where a ≠ b, then a and b 
+# are an amicable pair and each of a and b are called
+# amicable numbers. 
+
+# For example, the proper divisors of 220 are
+# 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore
+# d(220) = 284. The proper divisors of 284 are  1, 2, 4, 71 and 142;
+# so d(284) = 220.
+
+# Evaluate the sum of all the amicable numbers under 10000. 
+
+function sum_proper_divisors(n)
+    return sum(filter(x -> n % x == 0, 1:n-1))
+end
+
+function euler_0021(n)
+    d = x -> sum_proper_divisors(x)
+    return sum(filter(x -> x != d(x) && x == d(d(x)), 1:n-1))
+end 
+
 # Euler_0020 Factorial digit sum
 # n! means n × (n − 1) × ... × 3 × 2 × 1
 #
--- euler_01xx/tests/test_euler_002x.jl
+++ euler_01xx/tests/test_euler_002x.jl
@@ -5,6 +5,12 @@ using Test
 include("../euler_002x.jl")
 
 @testset "Euler_0020" begin
+    @test sum_proper_divisors(220) == 284
+    @test sum_proper_divisors(284) == 220
+    @test euler_0021(10000) == 31626
+end
+
+@testset "Euler_0020" begin
     @test euler_0020(10) == 27
     @test euler_0020(100) == 648   
 end
